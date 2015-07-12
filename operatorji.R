d6f5946317d055b3b0c70071cff795383c6722ad
Aljosa Vodopija <aljosa.vodopija@gmail.com>
2015-07-12 09:55:57 +0200

    Dodal Laplaceov operator

--- operatorji.R
+++ operatorji.R
@@ -1,14 +1,14 @@
-#DIV izracuna divergenco F=(F1,F2) (vektorsko polje vetra)
-#Prejme metriko X (jakost zonalnega vetra) in matriko Y (jakost meridionalnega vetra),
-#pri tem velja dim(Y)=dim(X). 
-#Izraèuna matriki F1x in F2y ter vrne matriko D = F1x + F2y. 
-#lahko dodamo z-to komponento [x,y,z]
-
-DIV = function(X,Y){
-
-#zacetne nastavitve
+#Zacetne nastavitve
 hx = 0.9 #longitude razdalja
 hy = 1 #latitude razdalja
+
+
+#DIV izracuna diskretno divergenco F=(F1,F2) (vektorsko polje vetra)
+#Prejme metriko X (jakost zonalnega vetra) in matriko Y (jakost meridionalnega vetra),
+#pri tem velja dim(Y)=dim(X). 
+#Izraèuna matriki F1x in F2y ter vrne matriko F1x + F2y. 
+
+DIV = function(X,Y){
 
 return(odvodx(X,hx)+odvody(Y,hy))
 }
@@ -19,14 +19,19 @@ return(odvodx(X,hx)+odvody(Y,hy))
 
 GRADF = function(M,X,Y){
 
-#zacetne nastavitve
-hx = 0.9 #longitude razdalja
-hy = 1 #latitude razdalja
-
 return(odvodx(M,hx)*X+odvody(M,hy)*Y)
 }
 
-#odvod vektorskega polja F po x (Fx), pri tem uporablja sim. dif.
+#Diskretiziran Laplaceov operator skalarnega polja
+#Prejme matriko M (skalarno polje)
+#Izracuna Mxx in Myy ter vrne Mxx + Myy
+
+LAPLACE = function(M){
+ 
+return(odvodxx(M,hx)+odvodyy(M,hy))
+}
+
+#Odvod skalarnega polja F po x (Fx), pri tem uporablja sim. dif.
 
 odvodx = function(F,hx){
 
@@ -35,13 +40,13 @@ n = dim(F)[2]
 Fx = matrix(1:(n*m),m)
 
 Fx[,2:(n-1)] = 1/(2*hx)*(F[,3:n]-F[,1:(n-2)])
-Fx[,1] = 1/hx*(F[,2]-F[,1])
-Fx[,n] = 1/hx*(F[,n]-F[,(n-1)])
+Fx[,1] = 1/(2*hx)*(F[,2]-F[,1])
+Fx[,n] = 1/(2*hx)*(F[,n]-F[,(n-1)])
 
 return(Fx)
 }
 
-#odvod vektorskega polja F po y (Fy), pri tem uporablja sim. dif.
+#Odvod skalarnega polja F po y (Fy), pri tem uporablja sim. dif.
 
 odvody = function(F,hy){
 
@@ -50,8 +55,37 @@ n = dim(F)[2]
 Fy = matrix(1:(n*m),m)
 
 Fy[2:(m-1),] = 1/(2*hy)*(F[1:(m-2),]-F[3:m,])
-Fy[1,] = 1/hy*(F[1,]-F[2,])
-Fy[m,] = 1/hy*(F[(m-1),]-F[m,])
+Fy[1,] = 1/(2*hy)*(F[1,]-F[2,])
+Fy[m,] = 1/(2*hy)*(F[(m-1),]-F[m,])
 
 return(Fy)
+}
+
+#Drugi odvod skalrnega polja F po x (Fxx)
+
+odvodxx = function(F,hx){
+
+m = dim(F)[1]
+n = dim(F)[2]
+Fxx = matrix(1:(n*m),m)
+
+Fxx[,2:(n-1)] = 1/(hx^2)*(F[,3:n]+F[,1:(n-2)]-2*F[,2:(n-1)])
+Fxx[,1] = 1/(hx^2)*(F[,2]-F[,1])
+Fxx[,n] = 1/(hx^2)*(F[,(n-1)]-F[,n])
+
+return(Fxx)
+}
+
+#Drugi odvod skalarnega polja F po y (Fyy)
+
+odvodyy = function(F,hy){
+m = dim(F)[1]
+n = dim(F)[2]
+Fyy = matrix(1:(n*m),m)
+
+Fyy[2:(n-1),] = 1/(hy^2)*(F[3:n,]+F[1:(n-2),]-2*F[2:(n-1),])
+Fyy[1,] = 1/(hy^2)*(F[2,]-F[1,])
+Fyy[n,] = 1/(hy^2)*(F[(n-1),]-F[n,])
+
+return(Fyy)
 }
