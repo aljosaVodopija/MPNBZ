cc911f03893d7670c8622379db821ea00dde43d3
Matija Pretnar <matija@pretnar.info>
2016-02-04 14:46:38 +0100

    Uredil datoteke

---
+++ funkcije.r
@@ -0,0 +1,194 @@
+#Zacetne nastavitve
+hx = 0.9 #longitude razdalja
+hy = 1 #latitude razdalja
+
+
+#DIV izracuna diskretno divergenco F=(F1,F2) (vektorsko polje vetra)
+#Prejme metriko X (jakost zonalnega vetra) in matriko Y (jakost meridionalnega vetra),
+#pri tem velja dim(Y)=dim(X). 
+#Izračuna matriki F1x in F2y ter vrne matriko F1x + F2y. 
+
+DIV = function(X,Y){
+
+return(odvodx(X,hx)+odvody(Y,hy))
+}
+
+#GRADF izracuna s. p. med grad(m)*F (m skalarno polje, F vektorsko polje)
+#Prejme metriko M in matriki X, Y  
+#Izračuna matriki Mx, My in vrne iskani s. p. 
+
+GRADF = function(M,X,Y){
+
+return(odvodx(M,hx)*X+odvody(M,hy)*Y)
+}
+
+#Diskretiziran Laplaceov operator skalarnega polja
+#Prejme matriko M (skalarno polje)
+#Izracuna Mxx in Myy ter vrne Mxx + Myy
+
+LAPLACE = function(M){
+ 
+return(odvodxx(M,hx)+odvodyy(M,hy))
+}
+
+#Odvod skalarnega polja F po y (Fy), pri tem uporablja sim. dif.
+
+odvody = function(F,h){
+
+n = dim(F)[1]
+m = dim(F)[2]
+Fy = matrix(1:(n*m),n)
+
+Fy[,2:(m-1)] = 1/(2*h)*(F[,3:m]-F[,1:(m-2)])
+Fy[,1] = 1/(2*h)*(F[,2]-F[,1])
+Fy[,m] = 1/(2*h)*(F[,m]-F[,(m-1)])
+
+return(Fy)
+}
+
+#Odvod skalarnega polja F po x (Fx), pri tem uporablja sim. dif.
+
+odvodx = function(F,h){
+
+n = dim(F)[1]
+m = dim(F)[2]
+Fx = matrix(1:(n*m),n)
+
+Fx[2:(n-1),] = 1/(2*h)*(F[3:n,]-F[1:(n-2),])
+Fx[1,] = 1/(2*h)*(F[2,]-F[1,])
+Fx[n,] = 1/(2*h)*(F[n,]-F[(n-1),])
+
+return(Fx)
+}
+
+#Drugi odvod skalrnega polja F po y (Fyy)
+
+odvodyy = function(F,h){
+
+n = dim(F)[1]
+m = dim(F)[2]
+Fyy = matrix(1:(n*m),n)
+
+Fyy[,2:(m-1)] = 1/(h^2)*(F[,3:m]+F[,1:(m-2)]-2*F[,2:(m-1)])
+Fyy[,1] = 1/(h^2)*(F[,2]-F[,1])
+Fyy[,m] = 1/(h^2)*(F[,(m-1)]-F[,m])
+
+return(Fyy)
+}
+
+#Drugi odvod skalarnega polja F po x (Fxx)
+
+odvodxx = function(F,h){
+
+n = dim(F)[1]
+m = dim(F)[2]
+Fxx = matrix(1:(n*m),n)
+
+Fxx[2:(n-1),] = 1/(h^2)*(F[3:n,]+F[1:(n-2),]-2*F[2:(n-1),])
+Fxx[1,] = 1/(h^2)*(F[2,]-F[1,])
+Fxx[n,] = 1/(h^2)*(F[(n-1),]-F[n,])
+
+return(Fxx)
+}
+
+naloziZivali <- function(datotekaPremikov, izpustiVrstice) {
+    premiki <- read.table(datotekaPremikov, fill = TRUE)
+    premiki <- premiki[izpustiVrstice:length(premiki[, 1]),]
+
+    obcine <- sapply(premiki$V2, as.character)
+    obcine <- iconv(obcine, to="windows-1251")
+    prestej <- premiki$V7
+    prestej <- data.matrix(prestej)
+    bum <-iconv(obcine, to="windows-1251")
+    bum <- as.data.frame(table(bum))
+
+    stevilo <-  rep(0, length(bum$bum))
+
+    for(i in 1:length(bum$bum)){
+      stevilo[i] = sum(na.omit(as.numeric(prestej[which(obcine==bum$bum[i])])))
+    }
+
+    lon <- rep(0, length(bum$bum))
+    lat <- rep(0, length(bum$bum))
+
+    for(i in 1:length(bum$bum)){
+      mesto <- bum$bum[i]
+      lon[i] <- as.numeric(geocode.cache(mesto)$lon)
+      lat[i] <- as.numeric(geocode.cache(mesto)$lat)
+    }
+
+    gospodarstva <- as.numeric(bum$Freq)
+
+
+    tabela <- data.frame(lon = lon, lat = lat, stevilo = stevilo, gospodarstva = gospodarstva)
+    rownames(tabela) <- as.character(bum$bum)
+    return(tabela)
+}
+
+narisi <- function(tabela, barva, faktor) {
+    ## zemljevid slovenije
+    newmap <- getMap(resolution = "high")
+    europe.limits <- geocode.cache(c("Salovci", "Črnomelj", "Lendava", "Kobarid", "Bovec", "Trst"))
+    plot(newmap, xlim= range(europe.limits$lon), ylim= range(europe.limits$lat), asp = 1)
+    points(tabela$lon, tabela$lat, pch=19, col=barva, cex = tabela$gospodarstva/faktor)
+
+}
+
+shraniZemljevid <- function(tabela, imeDatoteke, barva, faktor) {
+    lim<- geocode.cache(c("Salovci", "Črnomelj", "Lendava", "Kobarid", "Bovec", "Trst"))
+
+    lonn <- c(min(lim$lon), max(lim$lon)) #define our map's ylim
+    latt <- c(min(lim$lat), max(lim$lat)) #define our map's xlim
+    #markers = paste0("&markers=color:blue|label:S|",latt[1],", ",lonn[1],"&markers=color:",
+    #                 "green|label:G|",latt[2],", ",lonn[2])
+    markers <- ""
+    center = c(geocode.cache("Celje")$lat, geocode.cache("Ljubljana")$lon)  #tell what point to center on
+    zoom <- 8 
+    #### Drobnica grafični prikaz, razmerje 1:50
+    try(terrmap <- GetMap(center=center, zoom=zoom, markers=markers,maptype= "roadmap"), silent=TRUE)
+    png(imeDatoteke, type='cairo-png')
+    tmp <- PlotOnStaticMap(terrmap, tabela$lat, tabela$lon, cex=tabela$gospodarstva/faktor,pch=20,col=barva)
+    dev.off()
+}
+
+nastaviVeter <- function (u, N, M) {
+
+  dim <- c((N - 2) * 3, (M - 2) * 4)
+  X <- matrix(rep(0, dim[1] * dim[2]), dim[1])
+  for (i in 2:(N - 1)) {
+    for (j in 2:(M - 1)) {
+      B <- matrix(rep(0, 4*3), 3)
+
+      B[1, 1] <- 1/2*(u[(i - 1), (j - 1)] + u[i, j])
+      B[1, 2] <- 1/2*(u[(i - 1), j] + u[i, j])
+      B[1, 3] <- 1/2*(u[(i - 1), j] + u[i, j])
+      B[1, 4] <- 1/2*(u[(i - 1), (j + 1)] + u[i, j])
+
+      B[2, 1] <- 1/2*(u[i, (j - 1)] + u[i, j])
+      B[2, 2] <- u[i, j]
+      B[2, 3] <- u[i, j]
+      B[2, 4] <- 1/2*(u[i, (j + 1)] + u[i, j])
+
+      B[3, 1] <- 1/2*(u[(i + 1), (j - 1)] + u[i, j])
+      B[3, 2] <- 1/2*(u[(i + 1), j] + u[i, j])
+      B[3, 3] <- 1/2*(u[(i + 1), j] + u[i, j])
+      B[3, 4] <- 1/2*(u[(i + 1), (j + 1)] + u[i, j])
+
+      X[(3 * (i - 2) + 1):(3 * (i - 2) + 3), (4 * (j - 2) + 1):(4 * (j - 2) + 4)] <- B
+    }
+  }
+  return(X)
+}
+
+geocode.cache <- function(mesta) {
+  kode = read.csv('vmesni-podatki/koordinateKrajev.csv', row.names = 1)
+  for(mesto in mesta) {
+    mesto = toupper(mesto)
+    if(!mesto %in% rownames(kode)) {
+      polozaj = geocode(paste(mesto, "SLOVENIJA"))
+      kode[mesto, ] = polozaj
+    }
+  }
+  write.csv(kode, 'vmesni-podatki/koordinateKrajev.csv')
+  return(kode[toupper(mesta), ])
+}
--- predpriprava.r
+++ predpriprava.r
@@ -6,10 +6,85 @@
 # install.packages("rworldmap")
 # install.packages("rworldxtra")
 
-source("nastaviMatrikoNicel.R")
-neugodnePovrsine <- c(1, 2, 3, 4, 5, 11, 15, 17)
-maxVisina <- 1000
-nastaviMatrikoNicel(maxVisina, neugodnePovrsine)
+library(ncdf4)
+library(ggmap)
+library(rworldmap)
+library(RCurl)
+library(RJSONIO)
+library(plyr)
+library(plotrix)
+library(RgoogleMaps)
+library(raster)
 
-source("nastaviVreme.R")
+source('funkcije.r')
+
+nastaviMatrikoNicel <- function (maxVisina, neugodnePovrsine) {
+  #odpremo podatke
+  povrsine <- "vhodni-podatki/geo_em.d02.nc"
+  fid = ncdf4::nc_open(povrsine,write=FALSE)
+  lu = ncdf4::ncvar_get(fid, "LU_INDEX")[61:153,53:108] #LU index
+  visina = ncdf4::ncvar_get(fid, "HGT_M")[61:153,53:108]  # nadmorska visina
+  ncdf4::nc_close(fid)
+
+  #dimenzije
+  N <- 93
+  M <- 56
+  dim <- c((N - 2) * 3, (M - 2) * 4)
+  matrikaNicel <- matrix(rep(0, dim[1] * dim[2]), dim[1])
+
+  for (i in 2:(N - 1)){
+    for (j in 2:(M - 1)){
+      #preveri ali je visina visja od max oz. ali je povrsina neugodna in
+      #v pozitivnem primeru nastavi nicle
+      if((visina[i, j] < maxVisina) && !(lu[i, j] %in% neugodnePovrsine)){
+        matrikaNicel[(3*(i-2)+1):(3*(i-2)+3),(4*(j-2)+1):(4*(j-2)+4)] <- 1
+      }
+    }
+  }
+  save(matrikaNicel, file = "vmesni-podatki/matrikaNicel.RData")
+}
+
+nastaviMatrikoNicel(1000, c(1, 2, 3, 4, 5, 11, 15, 17))
+
+nastaviVreme <- function (podatkiVreme, stDni) {
+
+  N <- 93
+  M <- 56
+  dim <- c((N - 2) * 3, (M - 2) * 4)
+
+  zonalniVeter <- array(0, dim = c(dim[1], dim[2], stDni))
+  meridionalniVeter <- array(0, dim = c(dim[1], dim[2], stDni))
+  temperatura <- array(0, dim = c(dim[1], dim[2], stDni))
+
+  for (i in 1:stDni) {
+    fid <- ncdf4::nc_open(podatkiVreme, write = FALSE)
+    #preberemo vetrovni komponenti in temperaturo
+    ut <- ncdf4::ncvar_get(fid, "u10")[61:153, 53:108, ]
+    vt <- ncdf4::ncvar_get(fid, "v10")[61:153, 53:108, ]
+    tt <- ncdf4::ncvar_get(fid, "t2")[61:153, 53:108, ]
+    ncdf4::nc_close(fid)
+
+    u <- round(apply(ut, c(1,2), mean), 1)
+    v <- round(apply(vt, c(1,2), mean), 1)
+    t <- round(apply(tt, c(1,2), mean), 1)
+
+    zonalniVeter[, , i] <- round(3.6 * nastaviVeter(u, N, M), 1)
+    meridionalniVeter[, , i] <- round(3.6 * nastaviVeter(v, N, M), 1)
+    temperatura[, , i] <- round(nastaviVeter(t, N, M) - 273, 1)
+  }
+
+  save(zonalniVeter, file = "vmesni-podatki/zonalniVeter.RData")
+  save(meridionalniVeter, file = "vmesni-podatki/meridionalniVeter.RData")
+  save(temperatura, file = "vmesni-podatki/temperatura.RData")
+}
+
 nastaviVreme("vhodni-podatki/20150530.nc", 1)
+
+drobnica <- naloziZivali("vhodni-podatki/drobnica_stalez_3l.txt", 3)
+save(drobnica, file = 'vmesni-podatki/drobnica.RData')
+
+prasici <- naloziZivali("vhodni-podatki/prasici_stalez_3l.txt", 3)
+save(prasici, file = 'vmesni-podatki/prasici.RData')
+
+govedo <- naloziZivali("vhodni-podatki/govedo_3leta.txt", 5)
+save(govedo, file = 'vmesni-podatki/goveda.RData')
